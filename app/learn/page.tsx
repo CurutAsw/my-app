38622cb15fd80933473d44c8b8c9fafdb8792080
CurutAsw <nhayabusa0@gmail.com>
2026-04-20 12:27:18 +0800

    feat:newlearnpage

---
+++ app/learn/page.tsx
@@ -0,0 +1,15 @@
+export default function learn() {
+    return (
+        <div>
+            <div>
+                <img src="palette.svg" alt="Desing" />
+                <p>Desing</p>
+                </div>
+                <div>
+                    <p>Figma</p>
+                    <p>8 hours ago</p>
+            </div>
+        </div>
+
+    );
+}
