84bd493bae9c830957f04b514d4df7c10e63f8b9
CurutAsw <nhayabusa0@gmail.com>
2026-04-27 12:25:20 +0800

    update ccs

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,15 +1,16 @@
 export default function learn() {
     return (
-        <div>
-            <div>
+<section className="h-screen bg-gray-600 flex gap-4">
+        <div className="bg-purple-400 p-8 h-78 w-96 rounded-b-4xl flex flex-col justify-between">
+            <div className="flex gap-4">
                 <img src="palette.svg" alt="Desing" />
                 <p>Desing</p>
                 </div>
                 <div>
-                    <p>Figma</p>
+                    <p className="text-4xl">Figma</p>
                     <p>8 hours ago</p>
             </div>
         </div>
-
+</section>
     );
 }
